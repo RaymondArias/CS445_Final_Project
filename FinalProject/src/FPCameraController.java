7477f089a38740ec5b185defa6d7d2102f8a377e
RaymondArias <rarias2010@yahoo.com>
2016-10-26 19:20:05 -0700

    Added ability to move around environment

---
+++ FinalProject/src/FPCameraController.java
@@ -0,0 +1,175 @@
+
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.vector.Vector3f;
+
+public class FPCameraController {
+
+    private Vector3f position;
+    private Vector3f lPosition;
+    private float yaw;
+    private float pitch;
+    private Vector3Float me;
+
+    public FPCameraController(float x, float y, float z) {
+        this.position = new Vector3f(x, y, z);
+        this.lPosition = new Vector3f(x, y, z);
+        lPosition.x = 0f;
+        lPosition.y = 15.0f;
+        lPosition.z = 0.0f;
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3f position) {
+        this.position = position;
+    }
+
+    public Vector3f getlPosition() {
+        return lPosition;
+    }
+
+    public void setlPosition(Vector3f lPosition) {
+        this.lPosition = lPosition;
+    }
+
+    public float getYaw() {
+        return yaw;
+    }
+
+    public void setYaw(float yaw) {
+        this.yaw = yaw;
+    }
+
+    public float getPitch() {
+        return pitch;
+    }
+
+    public void setPitch(float pitch) {
+        this.pitch = pitch;
+    }
+
+    public Vector3Float getMe() {
+        return me;
+    }
+
+    public void setMe(Vector3Float me) {
+        this.me = me;
+    }
+
+    public void yaw(float amount) {
+        this.yaw += amount;
+    }
+
+    public void pitch(float amount) {
+        this.pitch -= amount;
+    }
+
+    public void walkForward(float distance) {
+        float xOffset = distance * (float) Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float) Math.cos(Math.toRadians(yaw));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+
+    public void walkBackwards(float distance) {
+        float xOffset = distance * (float) Math.sin(Math.toRadians(yaw));
+        float zOffset = distance * (float) Math.cos(Math.toRadians(yaw));
+        position.x += xOffset;
+        position.z -= zOffset;
+    }
+
+    public void strafeLeft(float distance) {
+        float xOffset = distance * (float) Math.sin(Math.toRadians(yaw - 90));
+        float zOffset = distance * (float) Math.cos(Math.toRadians(yaw - 90));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+
+    public void strafeRight(float distance) {
+        float xOffset = distance * (float) Math.sin(Math.toRadians(yaw + 90));
+        float zOffset = distance * (float) Math.cos(Math.toRadians(yaw + 90));
+        position.x -= xOffset;
+        position.z += zOffset;
+    }
+
+    public void moveUp(float distance) {
+        position.y -= distance;
+    }
+
+    public void moveDown(float distance) {
+        position.y += distance;
+    }
+
+    public void lookThrough() {
+        glRotatef(pitch, 1.0f, 0.0f, 0.0f);
+        glRotatef(yaw, 0.0f, 1.0f, 0.0f);
+        glTranslatef(position.x, position.y, position.z);
+    }
+
+    public void gameLoop() {
+        FPCameraController camera = new FPCameraController(0, 0, 0);
+        float dx = 0f;
+        float dy = 0f;
+        float dt = 0f;
+        float lastTime = 0f;
+        long time = 0;
+        float mouseSensitivity = 0.09f;
+        float movementSpeed = 0.35f;
+        Mouse.setGrabbed(true);
+        while (!Display.isCloseRequested()
+                && !Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+            time = Sys.getTime();
+            lastTime = time;
+
+            dx = Mouse.getDX();
+            dy = Mouse.getDY();
+            camera.yaw(dx * mouseSensitivity);
+            camera.pitch(dy * mouseSensitivity);
+            if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+                camera.walkForward(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+                camera.walkBackwards(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+                camera.strafeLeft(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+                camera.strafeRight(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+                camera.moveUp(movementSpeed);
+            }
+            if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
+                camera.moveDown(movementSpeed);
+            }
+            glLoadIdentity();
+            camera.lookThrough();
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            render();
+            Display.update();
+            Display.sync(60);
+        }
+        Display.destroy();
+    }
+
+    public void render() {
+        try {
+            glBegin(GL_QUADS);
+            glColor3f(1.0f, 0.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glEnd();
+        } catch (Exception e) {
+        }
+    }
+}
+
--- FinalProject/src/MainDriver.java
+++ FinalProject/src/MainDriver.java
@@ -1,70 +1,82 @@
+
 import java.util.ArrayList;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
-import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
-import static org.lwjgl.opengl.GL11.GL_NICEST;
-import static org.lwjgl.opengl.GL11.GL_PERSPECTIVE_CORRECTION_HINT;
-import static org.lwjgl.opengl.GL11.GL_PROJECTION;
-import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL11.glHint;
-import static org.lwjgl.opengl.GL11.glLoadIdentity;
-import static org.lwjgl.opengl.GL11.glMatrixMode;
-import static org.lwjgl.opengl.GL11.glOrtho;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
 
 public class MainDriver {
+    private FPCameraController fp;
+    private DisplayMode displayMode;
     //Method: start
     //Purpose: Intitiliazes the gui window and calls 
     //render to draw shapes 
+
     public void start() {
+        fp = new FPCameraController(0f, 0f, 0f);
         try {
             createWindow();
             initGL();
-            render();
+            fp.gameLoop();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
     /*
-    Method: createWindow
-    Purpose: Creates the gui window with initial size and title
-    */
+     Method: createWindow
+     Purpose: Creates the gui window with initial size and title
+     */
+
     private void createWindow() throws Exception {
         Display.setFullscreen(false);
-        Display.setDisplayMode(new DisplayMode(640, 480));
+        DisplayMode d[]
+                = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640
+                    && d[i].getHeight() == 480
+                    && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
         Display.setTitle("CS445 Final Project");
         Display.create();
     }
     /*
-    Method: initGL
-    Purpose: set openGL with intitial values
-    */
+     Method: initGL
+     Purpose: set openGL with intitial values
+     */
+
     private void initGL() {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(0, 640, 0, 480, 1, -1);
+        GLU.gluPerspective(100.0f, 
+                (float) displayMode.getWidth()/(float)displayMode.getHeight(),
+                0.1f, 300.0f);
         glMatrixMode(GL_MODELVIEW);
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     }
     /*
-    Method: render
-    Purpose: reads shape data from file and draws them to screen
-    */
+     Method: render
+     Purpose: reads shape data from file and draws them to screen
+     */
+
     private void render() {
         while (!Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
-            
-                
-                Display.update();
-                Display.sync(60);
-            
+
+            Display.update();
+            Display.sync(60);
+
         }
         Display.destroy();
     }
-    public static void main(String []args)
-    {
+
+    public static void main(String[] args) {
         MainDriver md = new MainDriver();
         md.start();
     }
-    
+
 }
